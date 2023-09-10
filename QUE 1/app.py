d4c93394efae491059f558dc028143a0f7d8bb82
Sumukh Srivastava <130910648+jardani1@users.noreply.github.com>
2023-09-10 12:16:49 +0530

    Add files via upload

---
+++ QUE 1/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+import requests
+import json
+import concurrent.futures
+
+app = Flask(__name__)
+
+def fetch_numbers(url):
+    try:
+        response = requests.get(url, timeout=0.5)
+        response.raise_for_status()
+        data = response.json()
+        return data.get('numbers', [])
+    except (requests.exceptions.RequestException, json.JSONDecodeError):
+        return []
+
+@app.route('/numbers', methods=['GET'])
+def get_numbers():
+    urls = request.args.getlist('url')
+
+    merged_numbers = []
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # Fetch numbers from each URL concurrently
+        futures = [executor.submit(fetch_numbers, url) for url in urls]
+
+        for future in concurrent.futures.as_completed(futures):
+            numbers = future.result()
+            merged_numbers.extend(numbers)
+
+    # Remove duplicates and sort the numbers
+    merged_numbers = sorted(set(merged_numbers))
+
+    return jsonify({'numbers': merged_numbers})
+
+if _name_ == '__main__':
+    app.run(port=8008)
